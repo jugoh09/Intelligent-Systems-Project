7b580e86ce573fa6668e9a2f767de1d27a857888
jugoh09 <95648750+jugoh09@users.noreply.github.com>
2022-02-22 23:30:27 -0600

    Updated version

---
+++ import_csv.py
@@ -0,0 +1,37 @@
+import csv
+import numpy as np
+import math
+
+# rows = []
+# with open("Food_Content.csv", 'r') as file:
+#     csvreader = csv.reader(file)
+#     header = next(csvreader)
+#     for row in csvreader:
+#         rows.append(row)
+# print(header)
+# print(rows)
+
+data = []
+with open('Food_Content.csv') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        data.append(row)
+
+food = input("What did you ate: ")
+col = [x[0] for x in data]
+
+if food in col:
+    for x in range(0, len(data)):
+        if food == data[x][0]:
+            print(data[x])
+
+else:
+    print("Food does not exist") 
+
+
+# M = np.array([["Carbs"],["Proteins"],["Fats"]])
+# M = np.array([[0.85,0,0.5],[0.05,0.95,0.5],[0.1,0.5,0.3]])
+# c = np.array([0.4,0.3,0.3])
+
+# print(M)
+#y = np.linalg.solve(M,c)
