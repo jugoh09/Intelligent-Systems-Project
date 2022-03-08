e4a2a4c1110da692982cc68bfedbd1c05bc01387
jugoh09 <95648750+jugoh09@users.noreply.github.com>
2022-03-08 12:39:52 -0600

    changes in cramers

--- updated_cramers_method.py
+++ updated_cramers_method.py
@@ -20,8 +20,8 @@ fats_f3 = grams_f3 * 0.002
 
 
 #M = np.array["Carbs","Proteins","Fats"]
-M = np.array([[0.6,-0.3,-0.3],[-0.4,0.7,-0.3],[-0.4,-0.3,0.7]])
-c = np.array([(-0.6 * carbs_f1) + (0.4 * proteins_f1) + (0.4 * fats_f1), (0.3 * carbs_f2) + (-0.7 * proteins_f2) + (0.3 * fats_f2), (0.3 * carbs_f3) + (0.3 * proteins_f3) + (-0.7 * fats_f3)])
+M = np.array([[0.6,-0.4,-0.4],[-0.3,0.7,-0.3],[-0.3,-0.3,0.7]])
+c = np.array([[(-0.6 * carbs_f1) + (0.4 * proteins_f1) + (0.4 * fats_f1)], [(0.3 * carbs_f2) + (-0.7 * proteins_f2) + (0.3 * fats_f2)], [(0.3 * carbs_f3) + (0.3 * proteins_f3) + (-0.7 * fats_f3)]])
 
 #M = np.array([[-0.6,0.3,0.3],[0.4,-0.7,0.3],[0.4,0.3,-0.7]])
 #M = np.array([[0.85,0,0.5],[0.05,0.95,0.4],[0.1,0.5,0.3]])
@@ -34,14 +34,16 @@ print(proteins_f1)
 print(fats_f1)
 
 print(carbs_f2)
+print(proteins_f2)
 print(fats_f2)
-print(proteins_f3)
+
 
 print(carbs_f3)
-print(proteins_f2)
+print(proteins_f3)
 print(fats_f3)
 
 
 print(y)
 print(c)
+print(M)
 
